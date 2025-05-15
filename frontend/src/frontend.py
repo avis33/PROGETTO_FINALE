f56ce34e282b1ecabcae062180f1d17421042472
Vincenzo Uccello <vincenzo.uc21@gmail.com>
2025-05-15 21:46:02 +0200

    Configurato dockerfile di frontend e backend e verificato che funzionassero

--- frontend/src/frontend.py
+++ frontend/src/frontend.py
@@ -9,4 +9,12 @@ BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
 
 @app.get("/")   
 def index(request:Request): 
-    return templates.TemplateResponse("index.html", {"request": request})
+    try:
+        response = requests.get(f"{BASE_URL}/") 
+        response.raise_for_status()
+        res = response.json()
+    except requests.RequestException as e:
+        res = None
+        print("Errore nella chiamata dell'api", e)
+    return templates.TemplateResponse("index.html", {"request":request, "ok": res})
+
